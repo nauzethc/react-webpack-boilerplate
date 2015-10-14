6f48b4015e28906c7b35ee85192fbbb80917647b
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2015-10-14 13:45:15 +0100

    Now RenderDOM is resposible of render components into DOM

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
 const root = document.createElement('div');
 document.body.appendChild(root);
-React.render(<App />, root);
+
+const opts = {
+  title: 'React',
+  items: [
+    { key: 'react',      name: 'React' },
+    { key: 'webpack',    name: 'Webpack' },
+    { key: 'hot-loader', name: 'Hot Loader' },
+    { key: 'stateless',  name: 'Stateless Components' },
+  ],
+};
+
+ReactDOM.render(<App {...opts} />, root);
