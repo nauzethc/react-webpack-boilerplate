a22cf3d2693c4e27904f1f600baf2fcfb2fe25b7
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2015-10-14 13:46:14 +0100

    Changed from ES6 'class' skeleton to 'createClass' React factory

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import Item from './Item.jsx';
 import '../styles/App.css';
 
 
-class App extends React.Component {
+const App = React.createClass({
+
+  propTypes: {
+    title: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        key: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired
+      })
+    ).isRequired,
+  },
 
   render() {
     return (
       <div className="app">
-        <h1>Hello, React!</h1>
+        <h1 className="title">Hello, {this.props.title}!</h1>
+        { this.renderItems() }
       </div>
     );
+  },
+
+  renderItems() {
+    if (this.props.items.length) {
+      return (
+        <ul className="items">
+          { this.props.items.map(({ key, name }) => <li key={key}><Item name={name} /></li>) }
+        </ul>
+      );
+    } else {
+      return <p className="empty">No items</p>;
+    }
   }
 
-}
+});
 
 export default App;
